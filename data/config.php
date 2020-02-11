27bee5cd71c57bcf8802219dbee2a9122d1bd5b2
root <root@CCPC-001.localdomain>
2020-02-11 13:39:41 +0630

    First commit

---
+++ data/config.php
@@ -0,0 +1,208 @@
+<?php
+return [
+    'cacheTimestamp' => 1581397678,
+    'database' => [
+        'driver' => 'pdo_mysql',
+        'host' => 'localhost',
+        'port' => '',
+        'charset' => 'utf8mb4',
+        'dbname' => 'espocrm',
+        'user' => 'espocrmuser',
+        'password' => 'root'
+    ],
+    'useCache' => true,
+    'recordsPerPage' => 20,
+    'recordsPerPageSmall' => 5,
+    'applicationName' => 'EspoCRM',
+    'version' => '5.8.2',
+    'timeZone' => 'UTC',
+    'dateFormat' => 'MM/DD/YYYY',
+    'timeFormat' => 'hh:mm a',
+    'weekStart' => 0,
+    'thousandSeparator' => ',',
+    'decimalMark' => '.',
+    'exportDelimiter' => ';',
+    'currencyList' => [
+        0 => 'USD'
+    ],
+    'defaultCurrency' => 'USD',
+    'baseCurrency' => 'USD',
+    'currencyRates' => [
+        
+    ],
+    'outboundEmailIsShared' => true,
+    'outboundEmailFromName' => 'EspoCRM',
+    'outboundEmailFromAddress' => '',
+    'smtpServer' => '',
+    'smtpPort' => 465,
+    'smtpAuth' => true,
+    'smtpSecurity' => 'SSL',
+    'smtpUsername' => 'admin',
+    'smtpPassword' => 'admin',
+    'language' => 'en_US',
+    'logger' => [
+        'path' => 'data/logs/espo.log',
+        'level' => 'WARNING',
+        'rotation' => true,
+        'maxFileNumber' => 30
+    ],
+    'authenticationMethod' => 'Espo',
+    'globalSearchEntityList' => [
+        0 => 'Account',
+        1 => 'Contact',
+        2 => 'Lead',
+        3 => 'Opportunity'
+    ],
+    'tabList' => [
+        0 => 'Account',
+        1 => 'Contact',
+        2 => 'Lead',
+        3 => 'Opportunity',
+        4 => 'Case',
+        5 => 'Email',
+        6 => 'Calendar',
+        7 => 'Meeting',
+        8 => 'Call',
+        9 => 'Task',
+        10 => '_delimiter_',
+        11 => 'Document',
+        12 => 'Campaign',
+        13 => 'KnowledgeBaseArticle',
+        14 => 'Stream',
+        15 => 'User'
+    ],
+    'quickCreateList' => [
+        0 => 'Account',
+        1 => 'Contact',
+        2 => 'Lead',
+        3 => 'Opportunity',
+        4 => 'Meeting',
+        5 => 'Call',
+        6 => 'Task',
+        7 => 'Case',
+        8 => 'Email'
+    ],
+    'exportDisabled' => false,
+    'adminNotifications' => true,
+    'adminNotificationsNewVersion' => true,
+    'adminNotificationsCronIsNotConfigured' => true,
+    'adminNotificationsNewExtensionVersion' => true,
+    'assignmentEmailNotifications' => false,
+    'assignmentEmailNotificationsEntityList' => [
+        0 => 'Lead',
+        1 => 'Opportunity',
+        2 => 'Task',
+        3 => 'Case'
+    ],
+    'assignmentNotificationsEntityList' => [
+        0 => 'Meeting',
+        1 => 'Call',
+        2 => 'Task',
+        3 => 'Email'
+    ],
+    'portalStreamEmailNotifications' => true,
+    'streamEmailNotificationsEntityList' => [
+        0 => 'Case'
+    ],
+    'streamEmailNotificationsTypeList' => [
+        0 => 'Post',
+        1 => 'Status',
+        2 => 'EmailReceived'
+    ],
+    'emailNotificationsDelay' => 30,
+    'emailMessageMaxSize' => 10,
+    'notificationsCheckInterval' => 10,
+    'maxEmailAccountCount' => 2,
+    'followCreatedEntities' => false,
+    'b2cMode' => false,
+    'restrictedMode' => false,
+    'theme' => 'HazyblueVertical',
+    'massEmailMaxPerHourCount' => 100,
+    'massEmailVerp' => false,
+    'personalEmailMaxPortionSize' => 50,
+    'inboundEmailMaxPortionSize' => 50,
+    'authTokenLifetime' => 0,
+    'authTokenMaxIdleTime' => 120,
+    'userNameRegularExpression' => '[^a-z0-9\\-@_\\.\\s]',
+    'addressFormat' => 1,
+    'displayListViewRecordCount' => true,
+    'dashboardLayout' => [
+        0 => (object) [
+            'name' => 'My Espo',
+            'layout' => [
+                0 => (object) [
+                    'id' => 'default-activities',
+                    'name' => 'Activities',
+                    'x' => 2,
+                    'y' => 2,
+                    'width' => 2,
+                    'height' => 4
+                ],
+                1 => (object) [
+                    'id' => 'default-stream',
+                    'name' => 'Stream',
+                    'x' => 0,
+                    'y' => 0,
+                    'width' => 2,
+                    'height' => 4
+                ]
+            ]
+        ]
+    ],
+    'calendarEntityList' => [
+        0 => 'Meeting',
+        1 => 'Call',
+        2 => 'Task'
+    ],
+    'activitiesEntityList' => [
+        0 => 'Meeting',
+        1 => 'Call'
+    ],
+    'historyEntityList' => [
+        0 => 'Meeting',
+        1 => 'Call',
+        2 => 'Email'
+    ],
+    'cleanupJobPeriod' => '1 month',
+    'cleanupActionHistoryPeriod' => '15 days',
+    'cleanupAuthTokenPeriod' => '1 month',
+    'currencyFormat' => 2,
+    'currencyDecimalPlaces' => 2,
+    'aclStrictMode' => true,
+    'aclAllowDeleteCreated' => false,
+    'aclAllowDeleteCreatedThresholdPeriod' => '24 hours',
+    'inlineAttachmentUploadMaxSize' => 20,
+    'textFilterUseContainsForVarchar' => false,
+    'tabColorsDisabled' => false,
+    'massPrintPdfMaxCount' => 50,
+    'emailKeepParentTeamsEntityList' => [
+        0 => 'Case'
+    ],
+    'recordListMaxSizeLimit' => 200,
+    'noteDeleteThresholdPeriod' => '1 month',
+    'noteEditThresholdPeriod' => '7 days',
+    'emailForceUseExternalClient' => false,
+    'useWebSocket' => false,
+    'auth2FAMethodList' => [
+        0 => 'Totp'
+    ],
+    'personNameFormat' => 'firstLast',
+    'newNotificationCountInTitle' => false,
+    'isInstalled' => true,
+    'siteUrl' => 'http://192.168.56.1',
+    'passwordSalt' => '5f17a89947922de2',
+    'cryptKey' => 'f3c142f2fe816928ae0c9bd8e8efa181',
+    'hashSecretKey' => 'b5152382765e90e284f0553cb333f390',
+    'defaultPermissions' => [
+        'user' => 33,
+        'group' => 33
+    ],
+    'fullTextSearchMinLength' => 4,
+    'actualDatabaseType' => 'mysql',
+    'actualDatabaseVersion' => '5.7.29',
+    'googleMapsApiKey' => 'AIzaSyBRiyxSWp9CS89ITrITWgX8CCFT-b1VuDE',
+    'integrations' => (object) [
+        'GoogleMaps' => true
+    ]
+];
+?>
